805effc4ff9898c4847744f3cb567c0caf2244af
Satoshi Jek <jks15satoshi@gmail.com>
2021-01-27 17:59:31 +0800

    Initialize project

---
+++ nonebot_plugin_cooldown/extra/time_format.py
@@ -0,0 +1,36 @@
+def time_format(timestamp: int, format='std') -> str:
+    '''
+    格式化输出剩余时间信息。
+
+    参数：
+    - `timestamp: int`：时间戳。
+
+    关键字参数：
+    - `format: str`：格式名称，可用的格式名称有：
+        * `std`：标准格式，以冒号分隔日、时、分、秒，例如 `05:04:03:02`；
+        * `zh`：中文格式，例如 `5天4小时3分2秒`。
+      默认值为 `std`。
+
+    返回：
+    - `str`：格式化的时间信息
+    '''
+    days = abs(timestamp) // 86400
+    hours = (abs(timestamp) - days * 86400) // 3600
+    minutes = (abs(timestamp) - days * 86400 - hours * 3600) // 60
+    seconds = abs(timestamp) - days * 86400 - hours * 3600 - minutes * 60
+
+    if format == 'std':
+        return (f'{str(days).zfill(2)}:{str(hours).zfill(2)}:'
+                f'{str(minutes).zfill(2)}:{str(seconds).zfill(2)}')
+    elif format == 'zh':
+        result = []
+        if days:
+            result.append(f'{days}天')
+        if hours:
+            result.append(f'{hours}小时')
+        if minutes:
+            result.append(f'{minutes}分')
+        if seconds or (not days and not hours and not minutes):
+            result.append(f'{seconds}秒')
+
+        return ''.join(result)
---
+++ nonebot_plugin_cooldown/rule.py
@@ -0,0 +1,29 @@
+from nonebot.adapters import Bot
+from nonebot.rule import Rule
+
+from . import cooldown
+
+
+def is_cooled_down(token: str, type='normal', **kwargs) -> Rule:
+    '''
+    检查冷却事件是否已经结束的规则。如果仍在生效则为 `False`，反之为 `True`。
+
+    参数：
+    - `token: str`：事件标签。
+
+    关键字参数：
+    - `type: str`：事件类型，默认为 `normal`。包括：
+        * `global`：全局冷却事件；
+        * `group`：群组冷却事件，需要额外的关键字参数 `group: int` 指定群组 ID；
+        * `normal`：一般冷却事件，需要额外的关键字参数 `group: int` 和 
+          `user: int` 分别指定群组 ID 和用户 ID；
+        * `user`：用户冷却事件，需要额外的关键字参数 `user: int` 指定用户 ID。
+
+    返回：
+    - `nonebot.rule.Rule`
+    '''
+    async def _is_cooled_down(bot: Bot) -> bool:
+        info = cooldown.get_event(token, type=type, **kwargs)
+        return not info.get('status')
+
+    return Rule(_is_cooled_down)
